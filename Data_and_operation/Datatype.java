19f94771497528f4f2038e413aa5ed0a00358829
lee myung ho <dl_audgh@naver.com>
2020-07-08 22:41:30 +0900

    자바 데이더 타입

---
+++ Data_and_operation/Datatype.java
@@ -0,0 +1,12 @@
+public class Datatype{
+	public static void main(String[] args) {
+		System.out.println(6);
+		System.out.println("six");
+		
+		System.out.println(6+6);
+		System.out.println("6"+"6");
+		System.out.println(6*6);
+//		System.out.println("6"*"6");
+		System.out.println("1111".length()); //문자열의 길이를 알려줌
+	}
+}
