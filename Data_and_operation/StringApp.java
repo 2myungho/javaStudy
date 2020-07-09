80d2f39a91ddea2aef2c7f788b93ff12f735b733
lee myung ho <dl_audgh@naver.com>
2020-07-09 23:10:49 +0900

    07-09emd

---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,16 @@
+public class StringApp {
+	public static void main(String[] args) {
+		System.out.println("Hello World"); //String
+	//	System.out.println('Hello World'); //작은 따음표는 Character
+		System.out.println("H");
+		
+		System.out.println("Hello "
+				+ "World");
+		//new line
+		System.out.println("Hello \nWorld"); // \n 줄바꿈
+		
+		//System.out.println("Hello "World""); 오류
+		//escape
+		System.out.println("Hello \"World\""); // \를 앞에 붙혀주면 뒤에 따라오는 따옴표는" 일반 문자 열이 된다 
+	}
+}
---
+++ Data_and_operation/StringOperation.java
@@ -0,0 +1,6 @@
+public class StringOperation {
+	public static void main(String[] args) {
+		System.out.println("Hello World".length()); //11
+		System.out.println("Hello, leezche ... bye. ".replace("leezche","egoing")); //replace는 문자를 바꾼다.
+	}
+}
