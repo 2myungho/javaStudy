6166eb27ba3fdd0c9adac8d63f9701c81aa0ffd3
lee myung ho <dl_audgh@naver.com>
2020-07-12 00:26:21 +0900

    생활코딩

---
+++ Data_and_operation/Letter.java
@@ -0,0 +1,9 @@
+public class Letter {
+	public static void main(String[] args) {
+		String name = "myungho";
+		System.out.println("Hello, "+name+"..args.myungho..");
+		
+		double VAT = 10.0;
+		System.out.println(VAT);
+	}
+}
